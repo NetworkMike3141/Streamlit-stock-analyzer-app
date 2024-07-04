fd14e758cf71c7810a8f90d913e9671a406a4cdc
Mihai <156333892+NetworkMike3141@users.noreply.github.com>
2024-07-04 10:29:42 +0000

    o

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import pandas as pd
 import plotly.graph_objects as go
 from psycopg2 import sql, OperationalError
 
-# Set page configuration
 st.set_page_config(
    page_title="Stock Data Viewer",
    page_icon="📈",
@@ -15,14 +14,12 @@ st.title('📈 Stock Data Viewer')
 
 st.write("View and analyze stock data including moving averages and real-time prices.")
 
-# Database credentials
 db_user = st.secrets["DB_USER"]
 db_password = st.secrets["DB_PASSWORD"]
 db_host = st.secrets["DB_HOST"]
 db_port = int(st.secrets["DB_PORT"])
 db_name = st.secrets["DB_NAME"]
 
-# Function to fetch ticker data
 def fetch_ticker_data(ticker):
     try:
         connection = psycopg2.connect(
@@ -48,7 +45,6 @@ def fetch_ticker_data(ticker):
         st.error(f"Database error: {e}")
         return None
 
-# User input for ticker symbol
 ticker = st.text_input('Enter the ticker symbol:', '').strip().upper()
 
 if ticker:
@@ -57,27 +53,22 @@ if ticker:
         st.write('Stock Data:')
         st.table(data)
         
-        # Prepare data for the bar chart
         labels = ['365-day MA', '180-day MA', '90-day MA', 'Real-time price']
         values = data.iloc[0, 1:].tolist()
         
-        # Set colors, blue for MAs and red for 'Real-time price'
         colors = ['blue', 'blue', 'blue', 'red']
         
-        # Create the figure
         fig = go.Figure()
         
-        # Add bars to the figure
         for label, value, color in zip(labels, values, colors):
             fig.add_trace(go.Bar(
                 x=[label],
                 y=[value],
                 name=label,
                 marker_color=color,
-                width=0.4  # Reduced bar width by 50%
+                width=0.4 
             ))
         
-        # Update layout
         fig.update_layout(
             title=f'Price and Moving Averages for {ticker}',
             xaxis_title='Metric',
@@ -87,7 +78,6 @@ if ticker:
             showlegend=False
         )
         
-        # Display the plot
         st.plotly_chart(fig)
     else:
         st.write(f'No data found for the ticker symbol: {ticker}')
